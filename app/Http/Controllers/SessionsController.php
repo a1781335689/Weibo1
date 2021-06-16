96359c1394a70ea15de62ae409a91bebca94892a
shiwen <1781335689@qq.com>
2021-06-16 08:54:14 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -14,15 +14,18 @@ class SessionsController extends Controller
     }
 
 
-    //attempt 方法会接收一个数组来作为第一个参数，该参数提供的值将用于寻找数据库中的用户数据
+    
     public function store(Request $request)
     {
-       $credentialsss = $this->validate($request, [
+    	// validate 方法来进行数据验证。validate 方法接收两个参数，第一个参数为用户的输入数据，第二个参数为该输入数据的验证规则
+       $credentials = $this->validate($request, [
            'email' => 'required|email|max:255',
            'password' => 'required'
        ]);
 
-       if (Auth::attempt($credentialsss)) {
+
+       //attempt 方法会接收一个数组来作为第一个参数，该参数提供的值将用于寻找数据库中的用户数据。方法可接收两个参数，第一个参数为需要进行用户身份认证的数组，第二个参数为是否为用户开启『记住我』功能的布尔值
+       if (Auth::attempt($credentials, $request->has('remember'))) {
            session()->flash('success', '欢迎回来！');
            return redirect()->route('users.show', [Auth::user()]);
        } else {
