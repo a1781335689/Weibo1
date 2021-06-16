61d48ea4febc1504ee3ec3ac856f8d8e3f5d4073
shiwen <1781335689@qq.com>
2021-06-16 07:25:49 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
     public function create()
     {
-        $regEntry = '123';
-        return view('users.create', compact('regEntry'));
+        return view('users.create');
     }
 
     public function show(User $user)
@@ -18,22 +18,25 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-
-    //validate 方法接收两个参数，第一个参数为用户的输入数据，第二个参数为该输入数据的验证规则
     public function store(Request $request)
     {
+        // validator 由 App\Http\Controllers\Controller 类中的 ValidatesRequests 进行定义，因此我们可以在所有的控制器中使用 validate 方法来进行数据验证。validate 方法接收两个参数，第一个参数为用户的输入数据，第二个参数为该输入数据的验证规则
         $this->validate($request, [
-            'name' => 'required|unique:users|max:50',
+            'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
 
+
+        // 用户模型 User::create() 创建成功后会返回一个用户对象，并包含新注册用户的所有信息
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
 
+        // 用户注册成功后自动登录
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
