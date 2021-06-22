19cdfa041386fabbacb37c15374d5db6ece81eb9
shiwen <1781335689@qq.com>
2021-06-22 11:00:19 +0000

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -47,7 +47,10 @@ class UsersController extends Controller
     // 登录成功后显示的页面
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+                           ->orderBy('created_at', 'desc')
+                           ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
 
